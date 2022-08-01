d69b217d1ad3ccd892077edc8566af5441d178fa
tinik <i.tarasevich@belvg.com>
2022-08-01 23:15:16 +0300

    Fix bug with one object

--- views/main/consumption.php
+++ views/main/consumption.php
@@ -167,8 +167,12 @@ $this->registerJs($js);
     <?= $form->field($model, 'uid', ['template' => '{input}'])->hiddenInput(); ?>
     <?php
     $objectOptionArr = [];
-    foreach ($objectsData['Objects']['Line'] as $object) {
-        $objectOptionArr[$object['UIDObject']] = $object['FullName'];
+    if (isset($objectsData['Objects']['Line']['UIDObject'])){
+        $objectOptionArr[$objectsData['Objects']['Line']['UIDObject']] = $objectsData['Objects']['Line']['FullName'];
+    } else {
+        foreach ($objectsData['Objects']['Line'] as $object) {
+            $objectOptionArr[$object['UIDObject']] = $object['FullName'];
+        }
     }
     ?>
     <div class="group large">
