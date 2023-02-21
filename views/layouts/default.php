8a935700f70216500a3b6932d2889c4522053021
89tinik <89.tinik@gmail.com>
2023-02-21 10:40:00 +0300

    Remove preloader pirammida link

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -64,7 +64,7 @@ IeAssets::register($this);
                     <ul>
                         <li><?= Html::a('Профиль потребителя', ['main/profile'], ['class' => 'ploader']) ?></li>
                         <?php if (!empty($piramida = \Yii::$app->controller->piramida)): ?>
-                            <li><a href="http://93.92.80.25:5001/Account/LoginExternal?login=<?= $piramida['name'] ?>&sessionId=<?= $piramida['id'] ?>" class="ploader" target="_blank">Перейти в ИСУ</a></li>
+                            <li><a href="http://93.92.80.25:5001/Account/LoginExternal?login=<?= $piramida['name'] ?>&sessionId=<?= $piramida['id'] ?>" target="_blank">Перейти в ИСУ</a></li>
                         <?php endif; ?>
                         <!-- class="active" -->
                         <!--                        <li class="children">-->
