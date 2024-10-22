f358b2116acbb7e5fd26c6ea33ddadd64f231cd0
89tinik <89.tinik@gmail.com>
2024-10-22 20:47:11 +0300

    fix filter invoice for admin

--- modules/admin/assets/AdminAssets.php
+++ modules/admin/assets/AdminAssets.php
@@ -9,11 +9,13 @@ class AdminAssets extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/admin.css?v=0.0'
+        'css/admin.css?v=1.0'
     ];
     public $js = [
+        'js/jquery-ui.js',
         '//cdn.tiny.cloud/1/eu1na6v7c1x7bw9ufnghv4tpz8jwds9r5j8a1rbee8mu0736/tinymce/6/tinymce.min.js',
-        'js/admin.js?v=1',
+        'js/datepicker-ru.js',
+        'js/admin.js?v=2',
     ];
     public $depends = [
         'yii\web\YiiAsset',
